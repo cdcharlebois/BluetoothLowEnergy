2735878ca4e97bba9fa7aaa5beaa1b04edd88363
Conner Charlebois <cdcharlebois@gmail.com>
2017-06-26 13:57:27 -0400

    add first pass of BLE

--- src/BluetoothLowEnergy/widget/BluetoothLowEnergy.js
+++ src/BluetoothLowEnergy/widget/BluetoothLowEnergy.js
@@ -16,40 +16,73 @@ define([
     "dojo/_base/event",
 
 
-], function (declare, _WidgetBase, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml, dojoEvent) {
+], function(declare, _WidgetBase, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml, dojoEvent) {
     "use strict";
 
-    return declare("BluetoothLowEnergy.widget.BluetoothLowEnergy", [ _WidgetBase ], {
+    return declare("BluetoothLowEnergy.widget.BluetoothLowEnergy", [_WidgetBase], {
 
-
+        // modeler
+        bluetoothName: null,
         // Internal variables.
         _handles: null,
         _contextObj: null,
 
-        constructor: function () {
+        constructor: function() {
             this._handles = [];
         },
 
-        postCreate: function () {
+        postCreate: function() {
             logger.debug(this.id + ".postCreate");
         },
 
-        update: function (obj, callback) {
+        update: function(obj, callback) {
             logger.debug(this.id + ".update");
 
             this._contextObj = obj;
             this._updateRendering(callback);
         },
 
-        resize: function (box) {
-          logger.debug(this.id + ".resize");
+        resize: function(box) {
+            logger.debug(this.id + ".resize");
         },
 
-        uninitialize: function () {
-          logger.debug(this.id + ".uninitialize");
+        uninitialize: function() {
+            logger.debug(this.id + ".uninitialize");
         },
 
-        _updateRendering: function (callback) {
+        _scanForBLEDevice: function(name) {
+            // find the device by name
+            console.log(ble);
+            if (ble && typeof ble.scan === "function") {
+                ble.scan(
+                    [this.bluetoothName],
+                    5,
+                    this._onDeviceFound(device),
+                    function(err) {
+                        console.log('there was an error');
+                        console.log(err);
+                    }
+                )
+            }
+        },
+
+        _onDeviceFound: function(deviceInfo) {
+            // connect to the device
+            ble.connect(
+                deviceInfo.id,
+                this._onDeviceConnect(data),
+                function(err) {
+                    console.log('device found, error connecting');
+                    console.log(err);
+                }
+            )
+        },
+
+        _onDeviceConnect: function(data) {
+            console.log(data);
+        },
+
+        _updateRendering: function(callback) {
             logger.debug(this.id + "._updateRendering");
 
             if (this._contextObj !== null) {
@@ -61,10 +94,10 @@ define([
             this._executeCallback(callback);
         },
 
-        _executeCallback: function (cb) {
-          if (cb && typeof cb === "function") {
-            cb();
-          }
+        _executeCallback: function(cb) {
+            if (cb && typeof cb === "function") {
+                cb();
+            }
         }
     });
 });
